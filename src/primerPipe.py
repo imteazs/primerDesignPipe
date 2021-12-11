cc9575cb673d75f63a1dd70800651e0dc687c04f
Imteaz Siddique <imteazs@gmail.com>
2021-12-10 20:21:42 -0800

    start building

--- src/primerPipe.py
+++ src/primerPipe.py
@@ -1,10 +1,13 @@
 import pandas as pd
+from Bio import SeqIO
 import primer3 as primer
 import argparse
 
 
-
 if __name__ == "__main__":
-    res = primer.calcHairpin('CCCCCATCCGATCAGGGGG')
-    print(res)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fastafile')
+    args = parser.parse_args()
+    fasta = args.fastafile
+    print(fasta)
 
