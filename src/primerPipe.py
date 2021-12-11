e363485e3dd7f78bbb971dd80bffab3022c88dfa
Imteaz Siddique <imteazs@gmail.com>
2021-12-10 21:25:47 -0800

    setting up primer design.

--- src/primerPipe.py
+++ src/primerPipe.py
@@ -3,15 +3,22 @@ from Bio import SeqIO
 import primer3 as primer
 import argparse
 
+def primerDesign(seqRec):
+
 
 if __name__ == "__main__":
+    '''
+    Creating argument parser so that command line arguments can be passed
+    - First for getting path of the fasta file
+    '''
     parser = argparse.ArgumentParser()
     parser.add_argument('--fastafile')
     args = parser.parse_args()
     fasta = args.fastafile
 
+    # creating SeqIO biopython object
     seq = SeqIO.parse(fasta, 'fasta')
 
-    for item in seq:
-        print(item)
+    for record in seq:
+        primer.designPrimers(record)
 
