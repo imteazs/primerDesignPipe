e355eebe97398b26a98bded814f521a22f200ee3
Imteaz Siddique <imteazs@gmail.com>
2021-12-10 22:25:08 -0800

    setting up primer design.

--- src/primerPipe.py
+++ src/primerPipe.py
@@ -6,11 +6,35 @@ import argparse
 def primerDesign(seqRec):
     inputDict = {
                     'SEQUENCE_ID': seqRec.id,
-                    'SEQUENCE_TEMPLATE': seqRec.seq,
+                    'SEQUENCE_TEMPLATE': str(seqRec.seq),
                     'SEQUENCE_INCLUDED_REGION': [36,342]
                 }
-    oligo = primer.designPrimers(inputDict)
-    print(oligo)
+    designSettings = {
+                        'PRIMER_OPT_SIZE': 20,
+                        'PRIMER_PICK_INTERNAL_OLIGO': 1,
+                        'PRIMER_INTERNAL_MAX_SELF_END': 8,
+                        'PRIMER_MIN_SIZE': 17,
+                        'PRIMER_MAX_SIZE': 35,
+                        'PRIMER_OPT_TM': 60.0,
+                        'PRIMER_MIN_TM': 57.0,
+                        'PRIMER_MAX_TM': 63.0,
+                        'PRIMER_MIN_GC': 20.0,
+                        'PRIMER_MAX_GC': 80.0,
+                        'PRIMER_MAX_POLY_X': 100,
+                        'PRIMER_INTERNAL_MAX_POLY_X': 100,
+                        'PRIMER_SALT_MONOVALENT': 50.0,
+                        'PRIMER_DNA_CONC': 50.0,
+                        'PRIMER_MAX_NS_ACCEPTED': 0,
+                        'PRIMER_MAX_SELF_ANY': 12,
+                        'PRIMER_MAX_SELF_END': 8,
+                        'PRIMER_PAIR_MAX_COMPL_ANY': 12,
+                        'PRIMER_PAIR_MAX_COMPL_END': 8,
+                        'PRIMER_PRODUCT_SIZE_RANGE': [[90,100]]
+    }
+    oligo = primer.designPrimers(inputDict, designSettings)
+    for key, value in oligo.items():
+        print(key, ':', value)
+
 
 if __name__ == "__main__":
     '''
