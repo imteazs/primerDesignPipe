188880d87c596b6281d5afc5e2ae4c24c510a2f8
Imteaz Siddique <imteazs@gmail.com>
2021-12-12 17:27:56 -0800

    pipeline almost set

--- src/primerPipe.py
+++ src/primerPipe.py
@@ -2,44 +2,68 @@ import pandas as pd
 from Bio import SeqIO
 import primer3 as primer
 import argparse
-import csv
+
 
 def primerDesign(seqRec, start, length):
+    """
+    :param seqRec: record from the fasta file
+    :param start: base position of interest
+    :param length: length of the window of interest
+    :return: primer3 data in the form of a dictionary
+    """
     inputDict = {
-                    'SEQUENCE_ID': seqRec.id,
-                    'SEQUENCE_TEMPLATE': str(seqRec.seq),
-                    'SEQUENCE_INCLUDED_REGION': [start,length]
-                }
+        'SEQUENCE_ID': seqRec.id,
+        'SEQUENCE_TEMPLATE': str(seqRec.seq),
+        'SEQUENCE_INCLUDED_REGION': [start, length]
+    }
     designSettings = {
-                        'PRIMER_OPT_SIZE': 20,
-                        'PRIMER_PICK_INTERNAL_OLIGO': 1,
-                        'PRIMER_INTERNAL_MAX_SELF_END': 8,
-                        'PRIMER_MIN_SIZE': 17,
-                        'PRIMER_MAX_SIZE': 35,
-                        'PRIMER_OPT_TM': 60.0,
-                        'PRIMER_MIN_TM': 57.0,
-                        'PRIMER_MAX_TM': 63.0,
-                        'PRIMER_MIN_GC': 20.0,
-                        'PRIMER_MAX_GC': 80.0,
-                        'PRIMER_MAX_POLY_X': 100,
-                        'PRIMER_INTERNAL_MAX_POLY_X': 100,
-                        'PRIMER_SALT_MONOVALENT': 50.0,
-                        'PRIMER_DNA_CONC': 50.0,
-                        'PRIMER_MAX_NS_ACCEPTED': 0,
-                        'PRIMER_MAX_SELF_ANY': 12,
-                        'PRIMER_MAX_SELF_END': 8,
-                        'PRIMER_PAIR_MAX_COMPL_ANY': 12,
-                        'PRIMER_PAIR_MAX_COMPL_END': 8,
-                        'PRIMER_PRODUCT_SIZE_RANGE': [[90,100]]
+        'PRIMER_OPT_SIZE': 20,
+        'PRIMER_PICK_INTERNAL_OLIGO': 1,
+        'PRIMER_INTERNAL_MAX_SELF_END': 8,
+        'PRIMER_MIN_SIZE': 17,
+        'PRIMER_MAX_SIZE': 35,
+        'PRIMER_OPT_TM': 60.0,
+        'PRIMER_MIN_TM': 55.0,
+        'PRIMER_MAX_TM': 65.0,
+        'PRIMER_MIN_GC': 20.0,
+        'PRIMER_MAX_GC': 80.0,
+        'PRIMER_MAX_POLY_X': 100,
+        'PRIMER_INTERNAL_MAX_POLY_X': 100,
+        'PRIMER_SALT_MONOVALENT': 50.0,
+        'PRIMER_DNA_CONC': 50.0,
+        'PRIMER_MAX_NS_ACCEPTED': 0,
+        'PRIMER_MAX_SELF_ANY': 12,
+        'PRIMER_MAX_SELF_END': 8,
+        'PRIMER_PAIR_MAX_COMPL_ANY': 12,
+        'PRIMER_PAIR_MAX_COMPL_END': 8,
+        'PRIMER_PRODUCT_SIZE_RANGE': [[80, 100]]
     }
     oligo = primer.designPrimers(inputDict, designSettings)
-    for key, value in oligo.items():
-        print(key, ',', value)
     return oligo
 
-def primerFilter(primerList):
+
+def genDF(primerList):
+    """
+    :param primerList: primer3 dictionary object
+    :return: preprocessed dataframe to be ready for further processing
+    """
     data = pd.DataFrame(primerList)
-    finalData = None
+    data = data.T
+    data.to_csv('~/Documents/testdata/test.csv')
+    finalData = pd.DataFrame(columns=['oligo', 'id', 'type', 'oligo_size', 'product_size'])
+    for index, row in data.iterrows():
+        rowname_split = row.name.split('_')
+
+        if 'sequence' in row.name.lower():
+            # ['PRIMER', 'LEFT', '0', 'SEQUENCE']
+            nrow = {'oligo' : data[0][index], 'id': rowname_split[2], 'type': rowname_split[1]}
+            finalData = finalData.append(nrow, ignore_index=True)
+
+        elif 'pair' in row.name.lower():
+            idx = finalData.index[finalData['id'] == rowname_split[2]].tolist()
+            finalData['product_size'][idx] = data[0][index]
+
+    finalData['oligo_size'] = finalData['oligo'].str.len()
     return finalData
 
 
@@ -54,6 +78,12 @@ if __name__ == "__main__":
     parser.add_argument('--fastafile', help='Path to fasta file')
     parser.add_argument('--included_region_start', help='start position of subregion', type=int)
     parser.add_argument('--included_region_length', help='length of subregion', type=int)
+    parser.add_argument('--mv_cations', help='Concentration of monovalent cations in mM', type=float)
+    parser.add_argument('--dv_cations', help='concentration of divalent cations in mM', type=float)
+    parser.add_argument('--dntp', help='concentration of dntps in uM', type=float)
+    parser.add_argument('--DNA', help='concentration of DNA in nM', type=float)
+    parser.add_argument('--anneal_temp', help='annealing temp in C', type=float)
+
     args = parser.parse_args()
     fasta = args.fastafile
     include_reg_start = args.included_region_start
@@ -70,7 +100,4 @@ if __name__ == "__main__":
     '''
     for record in seq:
         design = primerDesign(record, include_reg_start, include_reg_len)
-        #resultdf = primerFilter(design)
-
-
-
+        resultdf = genDF(design)
